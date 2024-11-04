177ca9dce1df86e1011e5d1e47441d69bb0491b5
GenarLoya <genarodour2104@gmail.com>
2024-11-03 18:40:36 -0600

    feat: df for models acuracy

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from get_df import get_df
+from pandas import DataFrame
 from naive_bayes_model import execute_naive_bayes
 from tree_classifier_model import execute_tree_selector
 from colorama import just_fix_windows_console
@@ -8,5 +9,13 @@ if __name__ == "__main__":
     df = get_df()
 
     # * Executing Models
-    execute_naive_bayes(df)
-    execute_tree_selector(df)
+    naive_bayes_accuracy = execute_naive_bayes(df)
+    tree_classifier_accuracy = execute_tree_selector(df)
+
+    df_acuracy = DataFrame(
+        {
+            "Model": ["Naive Bayes", "Tree Classifier"],
+            "Accuracy": [naive_bayes_accuracy, tree_classifier_accuracy],
+        }
+    )
+    print(df_acuracy)
