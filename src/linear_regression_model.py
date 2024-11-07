494a33d5b5f579c4e73480896f54c4861061c2a9
GenarLoya <genaro.loyadour@loadtrailmx.com>
2024-11-07 09:38:31 -0600

    fixing linear regression

--- src/linear_regression_model.py
+++ src/linear_regression_model.py
@@ -7,20 +7,23 @@ from colorama import just_fix_windows_console
 
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 
 def execute_linear_regression(df):
     X = df.drop("is_spam", axis=1)
     y = df["is_spam"]
 
-    print("---Variables---")
+    X_binary = X.map(lambda x: 1 if x > 0 else 0)
+
+    print("--- Variables ---")
     print("X:")
-    print(X.head())
+    print(X_binary.head())
     print("Y:")
     print(y.head())
 
     print("--- Train Test Split ---")
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.25, random_state=42
+        X_binary, y, test_size=0.25, random_state=42
     )
     print('--- Split ---')
     print('X_train:', X_train.shape)
@@ -37,6 +40,7 @@ def execute_linear_regression(df):
     print('--- Prediction ---')
     print('y_pred:', y_pred)
 
+    # Convertir predicciones continuas a binario
     y_pred_binary = [1 if pred > 0.5 else 0 for pred in y_pred]
 
     score = model.score(X_test, y_test)
