34e8253eb1f1ef319ac02ed16f0b90357b82f24c
Rull08 <rulolopza2603@gmail.com>
2024-12-09 09:31:55 -0600

    Cloud Chart Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import DoughnutChart from './DoughnutChart'
 import { Predictor } from './Predictor';
+import CloudGraph from '/CloudGraph.jpeg'
 import StackedAreaChart from './StackedAreaChart'
 
 const App: React.FC = () => {
@@ -14,17 +15,18 @@ const App: React.FC = () => {
           <StackedAreaChart />
         </div>
       </section>
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+      <section className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4 mb-4">
         <div className="bg-white rounded-lg shadow p-6">
           <DoughnutChart />
         </div>
         <div className="bg-white rounded-lg shadow p-6 cols-span-2 sm:cols-span-2">
           <h2 className="text-lg font-semibold text-gray-800">Gráfico de Nube</h2>
-        </div>
-        <div className="bg-white w-full rounded-lg shadow p-6 mb-4">
-          <h2 className="text-lg font-semibold text-gray-800">Detector de Spam</h2>
-          <Predictor />
-        </div>
+              <img className="items-center" src={CloudGraph} />
+          </div>
+          <div className="bg-white w-full rounded-lg shadow p-6 mb-4">
+              <h2 className="text-lg font-semibold text-gray-800">Detector de Spam</h2>
+              <Predictor />
+            </div>
       </section>
     </div>
   );
